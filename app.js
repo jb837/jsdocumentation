2fb397c0da83bb92485cc8b1bf31b65ba143d035
jason <jasonb.fea@gmail.com>
2025-12-05 14:39:15 +0000

    started second section

---
+++ app.js
@@ -0,0 +1,33 @@
+// Grammar and Types
+
+// declaration and value hoisting of variables
+// variable scope
+// object mutation with const
+// type coercion
+// converting strings to numbers parseInt parseFloat
+// Number() - parseInt use radix
+// empty items in an array - extra commas
+// numeric literals represented by
+// different integer literals
+// in different bases
+// hex, binary and octal to decimal base 10
+// exponent e - e with a number represents
+// the number of zeros
+// method definition within an object
+// use a numeric literal with bracket notation for an object's properties
+
+// NB - check this section Enhanced Object literals
+// NB - check this section Regex literals
+// NB - tagged templates needs to be checked
+// NB - special characters in javascript strings - ie \n
+
+// comparing string literal vs string datatypes
+// comparing object literals
+// assigning an object literal to a new object -
+/// the new object can reference the original object
+// length property on a string literal
+
+/// *****************************
+// Control flow and Error Handling
+
+// if statement with only one statement
